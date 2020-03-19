cc0b968d98b1dfb4202282f263d0659a322dfbd9
Priscila Souza <priscila.souza@bombeiros.mg.gov.br>
2020-03-19 12:21:25 -0300

    Product Collection Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+//use Illuminate\Http\Resources\Json\ResourceCollection;
+//use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+
+//class ProductCollection extends ResourceCollection
+// class ProductCollection extends Resource
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +19,16 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1- ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ?
+                round($this->reviews->sum('star')/$this->reviews->count(),2)
+                : 'No rating yet',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
     }
 }
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -3,8 +3,10 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+//use Illuminate\Http\Resources\Json\Resource;
 
 class ProductResource extends JsonResource
+//class ProductResource extends Resource
 {
     /**
      * Transform the resource into an array.
@@ -20,7 +22,7 @@ class ProductResource extends JsonResource
             'price' => $this->price,
             'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
             'discount' => $this->discount,
-            'totalPrice' => (1- ($this->discount/100)) * $this->price,
+            'totalPrice' => round((1- ($this->discount/100)) * $this->price, 2),
             'rating' => $this->reviews->count() > 0 ?
                 round($this->reviews->sum('star')/$this->reviews->count(),2)
                 : 'No rating yet',
