6d23c1f6163102e0674b7da8ae38c64c7caf83ba
Priscila Souza <priscila.souza@bombeiros.mg.gov.br>
2020-03-19 11:47:16 -0300

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,15 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => (1- ($this->discount/100)) * $this->price,
+            'rating' => $this->reviews->count() > 0 ?
+                round($this->reviews->sum('star')/$this->reviews->count(),2)
+                : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
